6b485a7fb27d673aaa7294677c44f778d3449ad2
jose david rincon angeles <angelnmara@hotmail.com>
2018-12-19 19:20:16 -0600

    terminado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,50 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ProductComponent } from './product/product.component';
 import { ProductAddComponent } from './product-add/product-add.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ProductEditComponent } from './product-edit/product-edit.component';
+
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+const appRoutes: Routes = [
+  {
+    path: 'products',
+    component: ProductComponent,
+    data: { title: 'Product List' }
+  },
+  {
+    path: 'product-details/:id',
+    component: ProductDetailComponent,
+    data: { title: 'Product Details' }
+  },
+  {
+    path: 'product-add',
+    component: ProductAddComponent,
+    data: { title: 'Product Add' }
+  },
+  {
+    path: 'product-edit/:id',
+    component: ProductEditComponent,
+    data: { title: 'Product Edit' }
+  },
+  { path: '',
+    redirectTo: '/products',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductComponent,
-    ProductAddComponent
+    ProductAddComponent,
+    ProductDetailComponent,
+    ProductEditComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
+    FormsModule,
     BrowserModule,
     AppRoutingModule,
     HttpClientModule
--- src/app/product-add/product-add.component.ts
+++ src/app/product-add/product-add.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { RestService } from '../rest.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-add',
   templateUrl: './product-add.component.html',
   styleUrls: ['./product-add.component.css']
 })
+
 export class ProductAddComponent implements OnInit {
 
-  constructor() { }
+  @Input() productData = { prod_name:'', prod_desc: '', prod_price: 0 };
+
+  constructor(public rest:RestService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
+  }
+
+  addProduct() {
+    this.rest.addProduct(this.productData).subscribe((result) => {
+      this.router.navigate(['/product-details/'+result._id]);
+    }, (err) => {
+      console.log(err);
+    });
   }
 
 }
